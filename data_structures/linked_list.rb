8e582717a45cccb557d04bbe36725d18a9159eaf
Lowell Mower <lowell.m.mower@gmail.com>
2016-04-10 11:00:30 -0400

    find and insert sorted methods on list

---
+++ data_structures/linked_list.rb
@@ -0,0 +1,40 @@
+require_relative 'node.rb'
+
+class List
+
+  attr_accessor :head
+
+  def initialize
+    @head = nil
+  end
+
+  def each
+    item = @head
+    while item
+      yield item
+      item = item.next
+    end
+  end
+
+  # normal insert into non-sorted list
+  def insert(data)
+    @head = Node.new(data, @head)
+  end
+
+  # insert into a sorted list
+  def insert_sorted(data)
+    return insert(data) if !@head || data <= @head.data
+    current = @head
+    while current.next && current.next.data < data
+      current = current.next
+    end
+    current.next = Node.new(data, current.next)
+  end
+
+  # because random access is not supported, the only traversal
+  # supported considers each node in turn
+  def find(val)
+    self.each {|n| return n if n.data == val }
+  end
+
+end
---
+++ data_structures/node.rb
@@ -0,0 +1,10 @@
+class Node
+
+  attr_accessor :data, :next
+
+  def initialize(data, next_node = nil)
+    @data = data
+    @next = next_node
+  end
+
+end
