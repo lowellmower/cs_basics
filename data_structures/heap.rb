818019125d467e803019df943cc0fef8da31b9c7
Lowell Mower <lowell.m.mower@gmail.com>
2016-04-11 14:13:50 -0400

    add notes for min/max heap and skeleton class

---
+++ data_structures/heap.rb
@@ -0,0 +1,47 @@
+# a heap is an abstract data structure similar to that of
+# a binary search tree. Like a BST, a heap is a collection
+# of nodes that have a data member value and at most, two
+# child nodes. The differentiating factor is that nodes in
+# a heap satisfy the heap property:
+# e.g. when p.value >= n.value for each child node n of a
+# parent p - - this example would be considered a max heap
+# EXAMPLE:
+#        5
+#       / \
+#      4   3
+# a min heap would be a heap where in a parent p never
+# exceeds the values of a child node n. This would be
+# known as a min heap
+# EXAMPLE:
+#          3
+#        /   \
+#       10    4
+#      / \   / \
+#     13 11 5   8
+# note that the root in a min heap has the smallest value
+# in the data structure and the root in a max heap is the
+# opposite, e.g. the larget value in the data structure
+
+class Heap
+
+  def initialize
+
+  end
+
+  def insert(data)
+
+  end
+
+  def find_max
+
+  end
+
+  def remove_max
+
+  end
+
+  def increase_key
+
+  end
+
+end
