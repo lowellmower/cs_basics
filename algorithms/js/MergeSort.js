570f8f1fb1fc87637cd782c5de430df9eed363f4
tomatagolo <tom@agolo.com>
2016-04-13 18:57:25 -0400

    add merge => selection sort

--- algorithms/js/MergeSort.js
+++ algorithms/js/MergeSort.js
@@ -22,6 +22,30 @@ function Merge(arr, lo, mid, hi){
   }
 };
 
+function SelectionMergeSort(arr, lo, hi){
+  if (lo == undefined) { lo = 0, hi = arr.length - 1; }
+  if (lo >= hi) { return; }
+  let mid = lo + Math.floor((hi-lo)/2);
+  SelectionMergeSort(arr, lo, mid);
+  SelectionMergeSort(arr, mid+1, hi);
+  SelectionMerge(arr, lo, mid, hi);
+};
+
+function SelectionMerge(arr, lo, mid, hi){
+  if (arr.slice(lo, hi+1).length >= 4){
+    Merge(arr, lo, mid, hi);
+  } else {
+    for (let i=lo; i<hi+1; i++){
+      for (let j=i; j>0 && arr[j] < arr[j-1]; j--){
+        let temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+      }
+    }
+  }
+};
+
 module.exports = {
   MergeSort,
+  SelectionMergeSort,
 };
