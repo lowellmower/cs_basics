5e26fd6aca34146c75b45894794f542b1b5faae4
Lowell Mower <lowell.m.mower@gmail.com>
2016-04-16 09:02:16 -0400

    add quick sort

--- algorithms/quick_sort.rb
+++ algorithms/quick_sort.rb
@@ -1,12 +1,16 @@
 def quick_sort(arr)
-  return if arr.size < 2
-  pivot = arr.size / 2
-  sub_arr1 = arr[0..pivot]
-  quick_sort(sub_arr1)
-  arr[0..pivot] = sub_arr1
+  return arr if arr.length <= 1
 
-  sub_arr2 = arr[pivot...arr.size]
-  quick_sort(sub_arr2)
-  arr[pivot...arr.size] = sub_arr2
-  arr
+  pivot_index = rand(arr.size)
+  pivot_value = arr.delete_at(pivot_index)
+
+  lesser = []
+  greater = []
+
+  arr.each { |x| x >= pivot_value ? greater << x : lesser << x}
+
+  return quick_sort(lesser) + [pivot_value] + quick_sort(greater)
+
 end
+
+puts quick_sort([2,5,8,1,3,6])
